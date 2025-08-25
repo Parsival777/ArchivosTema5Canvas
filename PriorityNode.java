e88c52ccbc64b4d23d82c91f0f0ca7eee2cab828
Alan <alananduaga07@gmail.com>
2025-08-25 14:58:59 -0600

    Add files via upload

---
+++ PriorityNode.java
@@ -0,0 +1,32 @@
+package ArchivosTema5;
+
+public class PriorityNode<T> {
+    private int priority;
+    private T data;
+
+    public PriorityNode(int priority, T data) {
+        this.priority = priority;
+        this.data = data;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "PriorityNode(priority=" + priority + ", data=" + data + ')';
+    }
+}
